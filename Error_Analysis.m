e49736a2b9bb473ae42d22a84caeccdec44e010c
Jcushman1116 <Jcushman1116@gmail.com>
2026-03-01 17:23:33 -0500

    Odering and Error Analysis Routine Added

---
+++ Error_Analysis.m
@@ -0,0 +1,12 @@
+function stats = Error_Analysis(p_exact, p_computed) 
+
+% compute residual
+residual = p_exact - p_computed;
+
+% Compute Statistics
+stats.infinityNorm = norm(residual, Inf); %infinity norm 
+stats.mean_err = mean(residual); %mean error 
+stats.variance = var(residual); % variance
+stats.rms_err = sqrt(mean(residual.^2)); %root mean square error 
+
+end
